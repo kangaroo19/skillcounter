3e3e47cc15a696f01488d757ac635ce2c2c7bb93
kangaroo19 <1000jjj@naver.com>
2024-10-09 18:21:02 +0900

    프로젝트 초기세팅

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Container, Content } from "@/ui/Layout";
+
+export default function Home() {
+  return (
+    <Container>
+      <Content>dfdfddfdfdfdfdf123df</Content>
+    </Container>
+  );
+}
